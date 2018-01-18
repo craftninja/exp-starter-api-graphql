5fdde62790e2a95d279c0a76c784a937e4f710b2
Emily Platzer <emilyplatzer@gmail.com>
2018-01-18 14:00:55 -0700

    Welcome route GQLified

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
+const { buildSchema } = require('graphql');
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.status(200);
-  res.send('oh hai');
-});
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = {
+  hello: () => {
+    return 'oh hai';
+  },
+};
+
+router.get('/', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 module.exports = router;
--- test/features/welcome.test.js
+++ test/features/welcome.test.js
@@ -9,9 +9,9 @@ describe('Root of API', () => {
   it('welcomes visitors', async () => {
     const res = await request(app)
       .get('/')
+      .send({query: '{ hello }'})
       .expect(200);
 
-    expect(res.text).toEqual('oh hai');
-    expect(res.body).toEqual({});
+    expect(res.body).toEqual({ data: { hello: 'oh hai' } });
   });
 });
